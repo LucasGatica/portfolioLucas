b4a293ba822976a7196951ea2b4f00f93aaa06a8
LucasGatica <30784336+LucasGatica@users.noreply.github.com>
2023-04-10 15:18:53 -0300

    quase funcional

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,7 +8,12 @@ import {yt} from "../assets"
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { isMobile } from "react-device-detect";
 
+const mobileFadeInVariant = {
+  hidden: {},
+  visible: {},
+};
 const ProjectCard = ({
   index,
   name,
@@ -18,15 +23,18 @@ const ProjectCard = ({
   source_code_link,
   yt_video
 }) => {
+
+  const fadeInVariant = isMobile ? mobileFadeInVariant : fadeIn("up", "spring", index * 0.5, 0.75);
+
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div variants={fadeInVariant}>
       <Tilt
         options={{
           max: 45,
           scale: 1,
-          speed: 450,
+          speed: 150,
         }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        className='bg-tertiary p-5 rounded-2xl sm:w-[390px] w-full'
       >
         <div className='relative w-full h-[230px]'>
           <img
@@ -87,6 +95,8 @@ const ProjectCard = ({
 };
 
 const Works = () => {
+  const fadeInVariant = isMobile ? mobileFadeInVariant : fadeIn("", "", 0.1, 1);
+
   return (
     <>
       <motion.div variants={textVariant()}>
@@ -96,12 +106,12 @@ const Works = () => {
 
       <div className='w-full flex'>
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+          variants={fadeInVariant}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Alguns projetos que desenvolvi, sinta-se à vontade para explorar e conhecer mais sobre meu trabalho, 
-          e não hesite em entrar em contato caso tenha interesse em discutir oportunidades de colaboração ou simplesmente 
-          trocar ideias sobre tecnologia e desenvolvimento. 
+          Alguns projetos que desenvolvi, sinta-se à vontade para explorar e conhecer mais sobre meu trabalho,
+          e não hesite em entrar em contato caso tenha interesse em discutir oportunidades de colaboração ou simplesmente
+          trocar ideias sobre tecnologia e desenvolvimento.
           Estou sempre aberto a novos desafios e aprendizados!
         </motion.p>
       </div>
